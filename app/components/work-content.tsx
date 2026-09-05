8ec1e2d7c8c5e07a90810891a694e450e9728a1e
amiraunyasopon <amiraunyasopon@gmail.com>
2026-09-05 13:23:47 -0700

    fix: 3d dog top margin fix

--- app/components/work-content.tsx
+++ app/components/work-content.tsx
@@ -4,7 +4,7 @@ import Scene from "./scene";
 export default function WorkContent() {
   return (
     <div className="flex flex-1 flex-col">
-      <div className="relative mt-8 h-78 w-full sm:h-102">
+      <div id="projects" className="relative h-78 w-full scroll-mt-8 sm:h-102">
         <Scene />
       </div>
       <header className="w-full pt-8 sm:pt-10">
